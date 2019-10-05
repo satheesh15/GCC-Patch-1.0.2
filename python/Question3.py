75c9b70079fa507f8998d73c1c5b8eb0c60113b1
satheesh15 <45646655+satheesh15@users.noreply.github.com>
2019-10-05 07:43:42 +0530

    Update Question3.py

--- python/Question3.py
+++ python/Question3.py
@@ -1,5 +1,38 @@
 # modify this function, and create other functions below as you wish
 def question03(scores, alice):
     # modify and then return the variable below
-    answer = -5
-    return answer
+    sc = set(scores)
+    scores = list(sc)
+    scores.sort()
+
+    ranks = []
+    for i in alice:
+	    ir = 1
+	    for j in scores:
+	    	if i<j:
+	    		ir +=1
+	    ranks.append(ir)
+
+    dic = {}
+    k = 0
+    v = 0
+    for i in ranks:
+    	if i in dic.keys():
+	    	j = dic[i]+1
+		    dic[i] = j
+	    	if j>v:
+		    	k = i
+			    v = j
+	    	elif j==v:
+		    	if i>k:
+			    	k = i
+    	else:
+	    	j = 1
+		    dic[i] = j
+    		if j>v:
+	    		k = i
+		    	v = j
+		    elif j==v:
+			    if i>k:
+				    k = i
+    return k
